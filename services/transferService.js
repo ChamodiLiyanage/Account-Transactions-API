0dcfd7d1864bd72d6715793bbbe2a4be1f927369
ChamodiLiyanage <it21828966@my.sliit.lk>
2024-08-19 23:14:46 +0530

    service implementation

---
+++ services/transferService.js
@@ -0,0 +1,41 @@
+const accounts = require("../models/accountModel");
+const { addTransaction } = require("../models/transactionModel");
+
+//Function to handle fund transfers
+const transferFunds = (
+  sourceAccountNumber,
+  destinationAccountNumber,
+  amount
+) => {
+  if (!accounts[sourceAccountNumber] && !accounts[destinationAccountNumber]) {
+    throw new Error("Both source and destination accounts do not exist");
+  }
+  if (!accounts[sourceAccountNumber]) {
+    throw new Error("Source account does not exist");
+  }
+  if (!accounts[destinationAccountNumber]) {
+    throw new Error("Destination account does not exist");
+  }
+  if (accounts[sourceAccountNumber].balance < amount) {
+    throw new Error("Insufficient funds");
+  }
+
+  //Perform the transfer
+  accounts[sourceAccountNumber].balance -= amount;
+  accounts[destinationAccountNumber].balance += amount;
+
+  //Log the transaction
+  const transaction = addTransaction(
+    sourceAccountNumber,
+    destinationAccountNumber,
+    amount
+  );
+
+  return {
+    sourceAccount: accounts[sourceAccountNumber],
+    destinationAccount: accounts[destinationAccountNumber],
+    transaction,
+  };
+};
+
+module.exports = { transferFunds };
